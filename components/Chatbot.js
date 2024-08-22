89c8cf111db7c470d5cd3c3f85ffae48fa22e8e3
Daniel Osagie <106236530+danielosagie341@users.noreply.github.com>
2024-08-22 22:21:09 +0100

    Update Chatbot.js

--- components/Chatbot.js
+++ components/Chatbot.js
@@ -53,7 +53,7 @@ const Chatbot = () => {
       })
     }
 
-    const response = await fetch("http://localhost:5000/gemini/send-response", options)
+    const response = await fetch("https://gemini-server-main-fitness-rings.onrender.com/gemini/send-response", options)
     const result = await response.text()
     return result
 
